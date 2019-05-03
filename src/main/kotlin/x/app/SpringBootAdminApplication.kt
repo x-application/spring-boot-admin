c070773ce9d7512ccf33eacf8b71ef1fff50973e
Jiahua.Zhang <i@iamee.me>
2019-05-04 01:07:44 +0800

    add properties

--- src/main/kotlin/x/app/SpringBootAdminApplication.kt
+++ src/main/kotlin/x/app/SpringBootAdminApplication.kt
@@ -1,8 +1,12 @@
 package x.app
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
+@EnableAdminServer
+@EnableEurekaClient
 @SpringBootApplication
 class SpringBootAdminApplication
 
